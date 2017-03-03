fd16ecf4316a1bf49df97b6f78dd3b9c41265551
Aleksey <aleksey.bestuzhev@gmail.com>
2017-03-03 07:40:51 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
+// $(document).ready(function () {
+//     $('#pagepiling').pagepiling({
+//         verticalCentered: false,
+//         css3: false,
+//         navigation: false,
+//     	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+//     	menu: '#myMenu',
+//     	scrollingSpeed: 500,
+//     	easing: 'swing'
+//     });
+//
+//     $('.step-button').click(function () {
+//     $.fn.pagepiling.moveSectionDown();
+// 	});
+//
+//     $("#myMenu").hover(function() {
+//     	$("#container").toggleClass('menu-push-toright');
+//     	$("#myMenu").toggleClass('menu-open');
+// 	});
+// });
+
+
 $(document).ready(function () {
-    $('#pagepiling').pagepiling({
-        verticalCentered: false,
-        css3: false,
-        navigation: false,
-    	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
-    	menu: '#myMenu',
-    	scrollingSpeed: 500,
-    	easing: 'swing'
-    });
+  $('#fullpage').fullpage({
+    verticalCentered: false,
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage', '6rdPage', '7rdPage', '8rdPage', '9rdPage', '10rdPage', '11rdPage'],
+    menu: '#vertical-nav',
+    scrollOverflow: true,
+    afterLoad: function (link, index) {
+      if (index == 3) {
+        $(".vertical-nav").css("transform", "translateX(0)");
+      } else if (index == 2) {
+        $(".vertical-nav").css("transform", "translateX(52px)");
+        $(".horizontal-nav").css("transform", "translateY(0)");
+      } else if (index == 1) {
+        $(".vertical-nav").css("transform", "translateX(52px)");
+        $(".horizontal-nav").css("transform", "translateY(-64px)");
+      }
+    },
+    resetSliders: true,
+     scrollOverflowReset: true,
+     paddingTop: "7vh",
+     paddingBottom: "10vh"
+  });
 
-    $('.step-button').click(function () {
-    $.fn.pagepiling.moveSectionDown();
-	});
+  $('.c-arrow-down').click(function(){
+    $.fn.fullpage.moveSectionDown();
+  });
 
-    $("#myMenu").hover(function() {
-    	// $("#container").toggleClass('menu-push-toright');
-    	// $("#myMenu").toggleClass('menu-open');
-	});
+  function uploadFile(target) {
+    document.getElementById("file-name").innerHTML = target.files[0].name;
+  }
 });
